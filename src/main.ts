6e67042e4d3a9a1fae4e7434a50c64685d1cb558
Leandro <leandro.ortega@darwoft.com>
2024-12-28 01:20:42 -0300

    instala logger y configservice

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const logger = new Logger();
+  const configService = app.get(ConfigService);
+
+  const port = configService.get('API_PORT');
+  const hostname = configService.get('HOSTNAME');
+
+  await app.listen(port, hostname, () => {
+    const address = 'http' + '://' + hostname + ':' + port + '/';
+    logger.log('Listening at ' + address, 'AppModule');
+  });
 }
 bootstrap();
